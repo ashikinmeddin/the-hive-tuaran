7313647da2a729f192ea7da0569c85a36b403a78
ashikinmeddin <ashikinmeddin@gmail.com>
2021-09-15 16:25:00 +0800

    Show order

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,9 +3,11 @@ class PagesController < ApplicationController
   end
 
   def menu
+    @listings = Listing.is_available
   end
 
   def order
+    @listings = Listing.is_available
   end
 
   def contact
